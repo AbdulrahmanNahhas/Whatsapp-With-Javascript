983d73e96794a9699145b6e0e371e4a04e865619
Abdulrahman <91866837+AbdulrahmanNahhas@users.noreply.github.com>
2022-06-19 15:55:34 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
 function send_handle() {
-  let num = document.getElementById("number").value;
+//   let num = document.getElementById("number").value;
 
-  let msg = document.getElementById("msg").value;
+//   let msg = document.getElementById("msg").value;
 
-  let name = document.getElementById("name").value;
+//   let name = document.getElementById("name").value;
 
   var win = window.open(
-    `https://wa.me/${num}?text=I%27m%20api%20msg%20hello%20${name}%20friend%20${msg}`,
+    `https://wa.me/905312911831?%D8%B9%D8%AF%D8%AF%20%D8%A7%D9%84%D9%88%D8%AC%D8%A8%D8%A7%D8%AA:%20%D9%A3%20%D8%A7%D9%84%D9%85%D9%86%D8%B7%D9%82%D8%A9:%20%D9%88%D8%A7%D8%AF%D9%8A%20%D8%B4%D9%87%D9%8A%D8%B1%20%D8%A7%D9%84%D8%B9%D8%AA%D8%A7%D8%A8%20%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A%20%D8%A7%D9%84%D9%88%D8%AC%D8%A8%D8%A7%D8%AA:%20%D8%A8%D8%B7%D9%8A%D8%AE%20%D9%85%D8%B4%D9%88%D9%8A%20+%20%D8%A8%D8%B7%D8%A7%D8%B7%D8%A7%20%D9%85%D9%82%D9%84%D9%8A%D8%A9%20+%20%D9%83%D9%88%D8%B3%D8%A9%20%D9%85%D8%AD%D8%B4%D9%8A`,
     "_blank"
   );
   win.focus();
